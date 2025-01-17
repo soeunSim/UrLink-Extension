2bd89bc7c5eb210cac6626764efd8cf35775721f
seonghoon <shee5032@naver.com>
2025-01-17 15:26:29 +0900

    feat: extension 설정

---
+++ public/service-worker.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+
+// Allows users to open the side panel by clicking on the action toolbar icon
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
