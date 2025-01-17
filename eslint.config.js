734714984b96c6d3bdcb8604de184a0da406d561
seonghoon <shee5032@naver.com>
2025-01-17 20:18:57 +0900

    chore: prettier에 trivago plugin 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
-import js from "@eslint/js"
-import globals from "globals"
-import react from "eslint-plugin-react"
-import reactHooks from "eslint-plugin-react-hooks"
-import reactRefresh from "eslint-plugin-react-refresh"
+import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
 
 export default [
   { ignores: ["dist"] },
