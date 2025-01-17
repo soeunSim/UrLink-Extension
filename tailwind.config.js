375f9c99160f8e21d37de1a706721003c54701ac
seonghoon <shee5032@naver.com>
2025-01-17 14:40:15 +0900

    chore: tailwindcss 설정

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        "mainBlack": "#000000",
+        "subGray": "#333333",
+      }
+    },
+  },
+  plugins: [],
+}
